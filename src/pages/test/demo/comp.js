dc53beb556c5a349f51d7d6df9fbe19a83058b94
tortoise1992 <772084015@qq.com>
2019-04-25 14:36:32 +0800

    优化拖拽组件页面结构

--- src/pages/test/demo/comp.js
+++ src/pages/test/demo/comp.js
@@ -1,45 +1,6 @@
 import React, { Component } from 'react'
-import { DragSource} from 'react-dnd';
 import {Icon} from 'antd'
-const rowSource = {
-    beginDrag(props) {
-        // console.log(props)
-        return {
-            // 开始拖拽的时候返回的对象可以在end的时候接收
-            id:props.item.id
-        };
-    },
-    endDrag(props, monitor) {
-        const item = monitor.getItem()
-        // console.log(item.item)
-        const dropResult = monitor.getDropResult()
-        // 由容器的drop方法里面传入
-        if (dropResult) {
-          alert(`You dropped ${item.id} into ${dropResult.name}!`)
-        }
-    },
-  };
-
-class DragItem extends Component{
-    render(){
-        const {
-            connectDragSource,
-            item
-        } = this.props;
-        return connectDragSource(<div style={{height:30,lineHeight:'30px',marginBottom:10,backgroundColor:'#09f',color:'#fff',cursor:'move'}} key={this.props.index}>
-            {item.title}
-        </div>)
-    }
-}
-
-DragItem=DragSource(
-    'row',
-    rowSource,
-    (connect,monitor) => ({
-      connectDragSource: connect.dragSource(),
-      isDragging: monitor.isDragging(),
-    }),
-  )(DragItem)
+import DragItem from './dragItem'
 class Comp extends Component {
     state={
         data:[
